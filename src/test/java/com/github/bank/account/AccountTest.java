91c8d914c6ddc7f1aca7d8effdc57db1f932d466
sbenbrahi <sarah.benbrahim-ext@apec.fr>
2018-06-21 16:28:51 +0200

    test main functions of the account, test fail

---
+++ src/test/java/com/github/bank/account/AccountTest.java
@@ -0,0 +1,26 @@
+package src.test.java.com.github.bank.account;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+import org.junit.Test;
+
+/**
+ * Created by sbenbrahi on 21/06/2018.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountTest {
+    @Test
+    public void deposit_ok() {
+    }
+
+    @Test
+    public void withdraawal_ok() {
+    }
+
+    @Test
+    public void statement_printing_ok() {
+    }
+}
